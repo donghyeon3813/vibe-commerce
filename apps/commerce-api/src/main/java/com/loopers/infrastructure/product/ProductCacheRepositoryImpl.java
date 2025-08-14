f2fe1b6d850c16f7ef6a6294689661cb57f33704
donghyeon3813 <ehdgusdl5753@gmail.com>
2025-08-15 00:07:10 +0900

    fix: redis Exception 처리 수정

--- apps/commerce-api/src/main/java/com/loopers/infrastructure/product/ProductCacheRepositoryImpl.java
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/product/ProductCacheRepositoryImpl.java
@@ -2,14 +2,18 @@ package com.loopers.infrastructure.product;
 
 import com.loopers.domain.product.ProductCacheRepository;
 import com.loopers.domain.product.ProductData;
+import com.loopers.support.error.CoreException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.List;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class ProductCacheRepositoryImpl implements ProductCacheRepository {
 
     private final ProductRedisRepository productRedisRepository;
@@ -17,12 +21,21 @@ public class ProductCacheRepositoryImpl implements ProductCacheRepository {
 
     @Override
     public List<ProductData> findByPageable(Long brandUid, Pageable pageable) {
-        return productRedisRepository.get(cacheKey.buildCacheKey(brandUid, pageable));
+        try {
+            return productRedisRepository.get(cacheKey.buildCacheKey(brandUid, pageable));
+        } catch (CoreException e){
+            log.error("Redis Exception", e);
+            return Collections.emptyList();
+        }
+
     }
 
     @Override
     public void setPageable(Long brandUid, Pageable pageable, List<ProductData> productData) {
-
-        productRedisRepository.set(cacheKey.buildCacheKey(brandUid, pageable), productData);
+        try {
+            productRedisRepository.set(cacheKey.buildCacheKey(brandUid, pageable), productData);
+        } catch (CoreException e){
+            log.error("Redis Exception", e);
+        }
     }
 }
