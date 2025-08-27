639f6a47b286faddfe56386fc8eb67c206c8a9a2
donghyeon3813 <ehdgusdl5753@gmail.com>
2025-08-27 23:36:08 +0900

    fix: 주문, 결제 transaction 분리

--- apps/commerce-api/src/main/java/com/loopers/application/order/OrderFacade.java
+++ apps/commerce-api/src/main/java/com/loopers/application/order/OrderFacade.java
@@ -31,49 +31,18 @@ import java.util.stream.Collectors;
 public class OrderFacade {
 
     private final UserService userService;
-    private final ProductService productService;
-
     private final OrderService orderService;
-    private final CouponIssueService couponIssueService;
-    private final CouponService couponService;
-
     private final PaymentProcessorFactory paymentProcessorFactory;
-    private final ApplicationEventPublisher applicationEventPublisher;
+    private final OrderProcessor orderProcessor;
 
-    @Transactional(noRollbackFor = CoreException.class)
     public OrderInfo.OrderResponse order(OrderCommand.Order order) {
         // 유저 확인
         UserModel user = userService.getUser(order.getUserId());
         if (user == null) {
             throw new CoreException(ErrorType.NOT_FOUND, "회원을 찾을 수 없습니다.");
         }
-        // product uid 추출
-        List<OrderCommand.Order.OrderItem> items = order.getItems();
-        Set<Long> productUidList = items.stream().map(OrderCommand.Order.OrderItem::getProductId).collect(Collectors.toSet());
-
-        //product 조회
-        List<Product> productList = productService.getProductsByProductUidsForUpdate(productUidList);
-        productService.checkProductConsistency(productUidList.size(), productList.size());
-
-        Coupon coupon = null;
-        Optional<CouponIssue> couponIssue;
-        Long couponId = 0L;
-        if (order.getCouponId() != null) {
-            couponIssue = Optional.ofNullable(couponIssueService.findByIdAndUseFlagForUpdate(order.getCouponId())
-                    .orElseThrow(() -> new CoreException(ErrorType.BAD_REQUEST, "사용할 수 없는 쿠폰입니다.")));
-            coupon = couponService.getCoupon(couponIssue.get().getCouponUid());
-            couponId = couponIssue.get().getId();
-            applicationEventPublisher.publishEvent(CouponIssueEvent.UseCouponIssueEvent.of(couponId));
-        }
-        //totalAmount 계산
-        BigDecimal totalAmount = orderService.calulateTotalAmount(items, productList, coupon);
-
-        //order 생성
-        OrderModel orderModel = orderService
-                .create(user.getId(), order.getItems(), totalAmount, order.getPhone(), order.getReceiverName(), order.getAddress(), couponId);
-
-        // 재고 차감
-        productService.deductQuantity(items, productList);
+        // 주문 진행
+        OrderModel orderModel = orderProcessor.orderProcess(order, user);
 
         // 결제 진행
         paymentProcessorFactory.getPaymentProcessor(order).process(orderModel, order, user);
