f520720573c0b998699b2af1de3670864bc3a34f
donghyeon3813 <ehdgusdl5753@gmail.com>
2025-08-28 23:29:48 +0900

    feat: 좋아요 수 증가 및 감소 이벤트 처리 추가

--- apps/commerce-api/src/main/java/com/loopers/infrastructure/productlike/ProductLikeJpaRepository.java
+++ apps/commerce-api/src/main/java/com/loopers/infrastructure/productlike/ProductLikeJpaRepository.java
@@ -5,8 +5,12 @@ import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface ProductLikeJpaRepository extends JpaRepository<ProductLike, Long> {
+    @Query(value = "SELECT pl FROM ProductLike pl WHERE pl.productId = :productUid")
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    Optional<ProductLike> findByIdForUpdate(@Param("productUid") Long productUid);
 }
