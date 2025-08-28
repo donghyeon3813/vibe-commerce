6391aa7d2bbdf39931a3dd6a0e6b4e162dae6855
donghyeon3813 <ehdgusdl5753@gmail.com>
2025-08-28 23:47:12 +0900

    fix: DeliveryEvent 비동기로 수행하도록 수정

--- apps/commerce-api/src/main/java/com/loopers/application/delivery/listener/DeliverListener.java
+++ apps/commerce-api/src/main/java/com/loopers/application/delivery/listener/DeliverListener.java
@@ -2,6 +2,7 @@ package com.loopers.application.delivery.listener;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
 public class DeliverListener {
 
     @EventListener
+    @Async
     public void orderDelivery(DeliveryEvent.OrderResultEvent event) {
         log.info("order delivery event: {}", event);
         //주문 정보를 받아서 외부 데이터 플랫폼에 보낸다.
@@ -20,6 +22,7 @@ public class DeliverListener {
     }
 
     @EventListener
+    @Async
     public void paymentDelivery(DeliveryEvent.PaymentResultEvent event) {
         log.info("payment delivery event: {}", event);
         //결제 정보를 받아서 외부 데이터 플랫폼에 보낸다.
